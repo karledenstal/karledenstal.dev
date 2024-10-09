faf99715a0fc5c02fd710f99e03f5487ff6c231d
Karl Edenstål <karl.edenstal@gmail.com>
2024-10-09 13:13:16 +0200

    fix: use the global yoe var instead

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const startYear = 2017
+export const yoe = new Date().getFullYear() - startYear
