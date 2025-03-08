893b5ca3761fdfa5818a119cc4acfc5f2edcd883
karledenstal <github@karledenstal.dev>
2025-03-08 11:41:07 +0100

    fix: update mailaddress

--- src/components/menu/MobileNav.tsx
+++ src/components/menu/MobileNav.tsx
@@ -1,6 +1,6 @@
-import clsx from 'clsx';
-import { Menu, X } from 'lucide-react';
-import { useState } from 'react';
+import clsx from "clsx";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 export const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,10 +9,10 @@ export const MobileNav = () => {
       <Menu className="cursor-pointer" onClick={() => setIsOpen((s) => !s)} />
       <div
         className={clsx(
-          'fixed top-0 left-0 transition-opacity bg-zinc-950 w-screen h-screen',
+          "fixed top-0 left-0 transition-opacity bg-zinc-950 w-screen h-screen",
           {
-            'opacity-100 flex': isOpen,
-            'opacity-0 hidden': !isOpen,
+            "opacity-100 flex": isOpen,
+            "opacity-0 hidden": !isOpen,
           }
         )}
       >
@@ -61,7 +61,7 @@ export const MobileNav = () => {
             <li className="group border-b border-b-zinc-800">
               <a
                 className="p-4 block text-xl font-mono text-center group-hover:bg-zinc-900"
-                href="mailto:karl.edenstal@gmail.com"
+                href="mailto:jobs@karledenstal.dev"
               >
                 Email
               </a>
